5dc41106eabc934423c79486f1aa98f5be816c7c
Nguyen Quang Minh <117517807+minhq2004@users.noreply.github.com>
2023-02-22 13:42:48 +0700

    Update TernarySearch.cpp

--- hw03/TernarySearch.cpp
+++ hw03/TernarySearch.cpp
@@ -11,24 +11,23 @@ int main () {
         cin >> a[i];
     }
     cout << "Nhap khoa tim kiem: ";
-    int x;
-    cin >> x;
-    int location;
+    int x; cin >> x;
     int i = 1;
     int j = n;
-    int m1;
-    int m2;
+    int m1, m2, location;
     while (i <= j ) {
     	m1 = i + (j - i)/3;
-		m2 = j - (j - i)/3;
-        if (x < a[m1]) j = m1 - 1;
+	m2 = j - (j - i)/3;
+        if (x < a[m1]) {
+	    j = m1 - 1;
+	}
         else if (x > a[m2]) {
             i = m2 + 1;
         }
         else {
-        	i = m1 + 1;
-        	j = m2 - 1;
-		}
+       	    i = m1 + 1;
+       	    j = m2 - 1;
+	}
     }
     if (x == a[i]) {
         location = i;
